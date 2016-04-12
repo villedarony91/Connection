741611bd3fd3de28725116256c6d53bb09aa61ba
wnk <rjlopez@DOMNTTSE>
2016-04-12 13:11:53 -0600

    connection without tnsnames and tracing

---
+++ Connection/Connection.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.OleDb;
+using System.Diagnostics;
+using System.Configuration;
+
+namespace Connection
+{
+    static class ConnectDB 
+    {
+
+        private static OleDbConnection getConnection()
+        {
+            Decryption dec = new Decryption();
+            OleDbConnection connection = new OleDbConnection();
+            connection.ConnectionString = dec.decryption(ConfigurationManager.ConnectionStrings["tests"].ToString());
+            return connection;
+        }
+
+        public static void ConnectToDB()
+        {
+            OleDbConnection connection = getConnection();
+            TextWriterTraceListener myTraceListener = new TextWriterTraceListener("trace.log", "myTraceListener");
+            myTraceListener.WriteLine("Starting trace");
+            using(connection){
+                try
+                {
+                    connection.Open();
+                    myTraceListener.WriteLine("State "+connection.State.ToString());
+                    Debug.WriteLine("** Conection opened **");
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine("debug");
+                    myTraceListener.WriteLine("A problem occurred during conection"+ex.ToString());
+                }
+
+            }
+            myTraceListener.Flush();
+            myTraceListener.Dispose();
+        }
+
+    }
+}
---
+++ Connection/Decryption.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Connection
+{
+    class Decryption
+    {
+
+
+
+        public string decryption(String encryptedString)
+        {
+            string skey = "IOJDGF123FGT1548IOJDGF123FGT1548";
+            string sIV = "IOJDGF123FGT1548";
+            byte[] IV = System.Text.Encoding.UTF8.GetBytes(sIV);
+            byte[] key = System.Text.Encoding.UTF8.GetBytes(skey);
+            return decryptString(encryptedString, key, IV);
+            
+        }
+
+        public string decryptString(String encryptedMessage, byte[] Key, byte[] IV)
+        {
+            try
+            {
+                byte[] cipherTextBytes = Convert.FromBase64String(encryptedMessage);
+                byte[] plainTextBytes = new byte[cipherTextBytes.Length];
+                Rijndael RijndaelAlg = Rijndael.Create();
+                MemoryStream memoryStream = new MemoryStream(cipherTextBytes);
+                CryptoStream cryptoStream = new CryptoStream(memoryStream,
+                                                             RijndaelAlg.CreateDecryptor(Key, IV),
+                                                             CryptoStreamMode.Read);
+                int decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+                memoryStream.Close();
+                cryptoStream.Close();
+                return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
+            }
+            catch (Exception e)
+            {
+                System.Windows.MessageBox.Show("Error durante proceso de desencriptación " + e.ToString());
+                return e.ToString();
+
+            }
+
+        }
+    }
+}
